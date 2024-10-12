71b146c11f28683507cf0960a56cbe355e43cda7
Ahmet Faruk Pala <65506131+afpthedev@users.noreply.github.com>
2024-10-12 21:36:29 +0300

    Güncellemeler

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 def index():
     kurban_durumu = {
         'asamalar': [
-            {'ad': 'Gönderi Alındı', 'tamamlandi': True},
-            {'ad': 'Transfer Sürecinde', 'tamamlandi': True},
-            {'ad': 'Dağıtıma Çıktı', 'tamamlandi': True},
-            {'ad': 'Teslim Edildi', 'tamamlandi': False}
+            {'ad': 'Gönderi Alındı', 'tamamlandi': True, 'icon': 'fa-sheep'},
+            {'ad': 'Hazırlanıyor', 'tamamlandi': True, 'icon': 'fa-hand-scissors'},
+            {'ad': 'Kesim Yapıldı', 'tamamlandi': True, 'icon': 'fa-cut'},
+            {'ad': 'Teslim Edildi', 'tamamlandi': False, 'icon': 'fa-truck'}
         ],
         'alici': 'ALT KAT BORAN TEL CEVAPSIZ',
         'adres': 'Örnek Mahallesi, Kurban Sokak No:123, Şehir/İlçe'
