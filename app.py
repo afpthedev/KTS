ea9fbbec7afe41a3b1ded337452083c81652921e
Ahmet Faruk Pala <65506131+afpthedev@users.noreply.github.com>
2024-10-12 21:29:49 +0300

    Güncellemeler

--- app.py
+++ app.py
@@ -4,14 +4,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # Örnek veri - gerçek uygulamada bu veri bir veritabanından gelebilir
     kurban_durumu = {
         'asamalar': [
-            {'ad': 'Kurban İsteğiniz Alındı', 'tamamlandi': True},
-            {'ad': 'Kurban Kesime Hazırlanıyor', 'tamamlandi': True},
-            {'ad': 'Kesildi', 'tamamlandi': False}
+            {'ad': 'İstek Alındı', 'tamamlandi': True, 'icon': 'clipboard-check'},
+            {'ad': 'Kesime Hazır', 'tamamlandi': True, 'icon': 'tasks'},
+            {'ad': 'Kesildi', 'tamamlandi': False, 'icon': 'cut'},
+            {'ad': 'Teslim Edildi', 'tamamlandi': False, 'icon': 'truck'}
         ],
-        'alici': 'ALT KAT BORAN TEL CEVAPSIZ'
+        'tamamlanan': 2,
+        'toplam': 4,
+        'alici': 'ALT KAT BORAN TEL CEVAPSIZ',
+        'adres': 'Örnek Mahallesi, Kurban Sokak No:123, Şehir/İlçe'
     }
     return render_template('index.html', kurban_durumu=kurban_durumu)
 
